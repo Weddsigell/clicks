82a0927ca9ba6058d9ba0a02498356cc647a7a75
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2024-12-23 23:04:40 +0500

    Убрал переменные окружения из функций

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from urllib.parse import urlparse
 from environs import Env
 import requests
-env = Env()
-env.read_env()
 
 
-def shorten_link(original_link):
-    requests.get(original_link).raise_for_status()
 
+def shorten_link(original_link, token, version):
     url = 'https://api.vk.ru/method/utils.getShortLink'
     params = {
-        "access_token": env.str("ACCESS_TOKEN"),
-        "v": env.float("VERSION"),
+        "access_token": token,
+        "v": version,
         "url": original_link,
     }
     response = requests.get(url, params=params)
@@ -20,14 +17,12 @@ def shorten_link(original_link):
     return response['response']['short_url']
 
 
-def count_clicks(short_url):
-    requests.get(short_url).raise_for_status()
-
+def count_clicks(short_url, token, version):
     link_key = urlparse(short_url).path
     url = 'https://api.vk.ru/method/utils.getLinkStats'
     params = {
-        "access_token": env.str("ACCESS_TOKEN"),
-        "v": env.float("VERSION"),
+        "access_token": token,
+        "v": version,
         "key": link_key[1:],
     }
     response = requests.get(url, params=params)
@@ -36,13 +31,11 @@ def count_clicks(short_url):
     return response['response']['stats'][0]['views']
 
 
-def is_shorten_link(original_link):
-    requests.get(original_link).raise_for_status()
-
+def is_shorten_link(original_link, token, version):
     url = 'https://api.vk.ru/method/utils.checkLink'
     params = {
-        "access_token": env.str("ACCESS_TOKEN"),
-        "v": env.float("VERSION"),
+        "access_token": token,
+        "v": version,
         "url": original_link,
     }
     response = requests.get(url, params=params)
@@ -52,15 +45,21 @@ def is_shorten_link(original_link):
 
 
 def main():
+    env = Env()
+    env.read_env()
+
+    access_token = env.str('ACCESS_TOKEN')
+    version = env.float('VERSION')
+
     original_link = input("Введите ссылку")
 
     try:
-        if is_shorten_link(original_link):
-            print('кол-во кликов: ', count_clicks(original_link))
+        if is_shorten_link(original_link, access_token, version):
+            print('кол-во кликов: ', count_clicks(original_link, access_token, version))
         else:
-            print('сокращенная ссылка', shorten_link(original_link))
+            print('сокращенная ссылка', shorten_link(original_link, access_token, version))
     except Exception as err:
-        print(err)
+        print('Ошибка, проверьте корректность ссылки')
 
 
 if __name__ == '__main__':
